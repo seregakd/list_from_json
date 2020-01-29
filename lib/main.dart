0ec2949455086324a55a424fe0f380a3789ffb78
sergey <seregakdg1@gmail.com>
2020-01-29 22:35:38 +0200

    add strukture wigets

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,38 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: Text("hi"),
+      debugShowCheckedModeBanner: false,
+      title: 'List from JSON',
+      home: HomePage(),
     );
   }
 }
 
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: new Text("List")
+      ),
+      body: _buildList(),
+    );
+  }
+
+  Widget _buildList() {
+    return new ListView.builder(
+//        padding: const EdgeInsets.all(16.0),
+//        itemCount: models.length,
+//        itemBuilder: (BuildContext context, int index){
+ //         return ItemList(itemModel: models[i], listItemNumber: i,
+ //             parentCount: refreshCount, parentCb: refreshCheckbox);
+ //       }
+    );
+  }
+}
