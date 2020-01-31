7889c79d6129aea3b42eac96bed426553df6f0eb
sergey <seregakdg1@gmail.com>
2020-02-01 01:23:41 +0200

    tests getData

--- lib/ui/GetData.dart
+++ lib/ui/GetData.dart
@@ -12,30 +12,20 @@ class _GetDataState extends State<GetData> {
   List dataUsers;
 //  Map <String, String> mapItems;
 //  Map <String, String> mapUsers;
+//  refactor to stateless wigets
 
   void getDataItemsFromJson() async {
-//    setState(() {
-//    String dataJson = await rootBundle.loadString('assets/DataItems.json');
-//    print ("dataItems" + dataJson);
-
     dataItems = json.decode(await rootBundle.loadString('assets/DataItems.json'));
-//    print ("dataItems" + dataItems.toString());
-//    });
     waitData();
   }
 
   void getDataUsersFromJson() async {
-//    return json.decode(await rootBundle.loadString('assets/DataUsers.json'));
-//    setState(() {
     dataUsers = json.decode(await rootBundle.loadString('assets/DataUsers.json'));
-//    print ("dataUsers" + dataUsers.toString());
-//    });
     waitData();
   }
 
   void waitData() {
     if (dataItems != null && dataUsers != null) {
-      //     setState(() {});
       print ("dataItems" + dataItems.toString());
       print ("dataUsers" + dataUsers.toString());
       Navigator.pushReplacementNamed(context, '/login',
@@ -44,7 +34,6 @@ class _GetDataState extends State<GetData> {
 //dataItems, dataUsers
     }
   }
-
 
   @override
   void initState(){
@@ -55,23 +44,15 @@ class _GetDataState extends State<GetData> {
   @override
   Widget build(BuildContext context) {
 //    return ProgressDialog();
-//    return Text("Wait loading data...");
+
     return Scaffold(
-      body: Text("Wait loading data..."),
+      appBar: AppBar(
+          title: Text("Wait loading data..."),
+      ),
     );
   }
 
 /*
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: Text("List")
-      ),
-      body: LoginPage(dataItems, dataUsers),
-    );
-  }
-
   Widget _buildList() {
     return new ListView.builder(
       itemCount: data == null ? 0 : data.length,
