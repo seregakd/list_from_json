e5b2c7b9763386b8453078ac48b47938b0b9d662
sergey <seregakdg1@gmail.com>
2020-02-01 00:15:15 +0200

    correct class GetData

--- lib/ui/GetData.dart
+++ lib/ui/GetData.dart
@@ -9,40 +9,72 @@ class GetData extends StatefulWidget {
 }
 
 class _GetDataState extends State<GetData> {
-  List data;
+  List dataItems = null;
+  List dataUsers = null;
+  Map <String, String> mapItems;
+  Map <String, String> mapUsers;
 
-  Future<String> getDataFromJson() async {
-    String dataJson = await rootBundle.loadString('assets/DataItems.json');
-
-    setState(() {
-      data = json.decode(dataJson);
-    });
+  Future<List> getDataItemsFromJson() async {
+//    return json.decode(await rootBundle.loadString('assets/DataItems.json'));
+//    setState(() {
+    dataItems = json.decode(await rootBundle.loadString('assets/DataItems.json'));
+//    });
+    waitData();
   }
 
+  Future<List> getDataUsersFromJson() async {
+//    return json.decode(await rootBundle.loadString('assets/DataUsers.json'));
+//    setState(() {
+    dataUsers = json.decode(await rootBundle.loadString('assets/DataUsers.json'));
+//    });
+    waitData();
+  }
+
+  void waitData() {
+    if (dataItems != null && dataUsers != null) {
+      //     setState(() {});
+      Navigator.pushReplacementNamed(context, '/login',
+          arguments: <String, List> {'dataItems': dataItems}
+      );
+//dataItems, dataUsers
+    }
+  }
+
+
   @override
-  void initState() {
-    getDataFromJson();
+  void initState(){
+    getDataItemsFromJson();
+    getDataUsersFromJson();
   }
 
   @override
   Widget build(BuildContext context) {
+//    return ProgressDialog();
+    return Text("Wait loading data...");
+  }
+
+/*
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: new Text("List")
+          title: Text("List")
       ),
-      body: _buildList(),
+      body: LoginPage(dataItems, dataUsers),
     );
   }
 
   Widget _buildList() {
     return new ListView.builder(
-        itemCount: data == null ? 0 : data.length,
-        cacheExtent: 100,
-        itemBuilder: (BuildContext context, int index) {
-          return new Card(
-            child: new Text(data[index]["title"]),
-          );
-        }
+      itemCount: data == null ? 0 : data.length,
+      cacheExtent: 100,
+      itemBuilder: (BuildContext context, int index){
+        return new Card(
+           child: Text(data[index]["title"]),
+        );
+      }
     );
   }
+*/
+
 }
