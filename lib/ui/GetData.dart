67683ccb2d16aa8941c313b99b5d9782bef84647
sergey <seregakdg1@gmail.com>
2020-02-15 20:01:28 +0200

    update to Future.wait

--- lib/ui/GetData.dart
+++ lib/ui/GetData.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:async';
 
 import 'package:list_from_json/model/DataFromJson.dart';
 
@@ -13,52 +14,49 @@ class GetData extends StatefulWidget {
 class _GetDataState extends State<GetData> {
   List itemsList;
   Map <String, String> usersMap = Map <String, String>();
-  bool _delay = true;
 
-  void getDataItemsFromJson() async {
+  Future <void> getDataItemsFromJson() async {
     var response = await http.get(
       Uri.encodeFull("https://gist.githubusercontent.com/seregakd/e8936fb85d4e985bc6e46f166afa35c6/raw/48f8597f61be948a2ccc11e1f47766a2a1890499/DataItems.json"),
     );
 
     if (response.statusCode == 200) {
       itemsList = json.decode(response.body);
+      print("OK - getDataItemsFromJson()");
     } else {
       print('Request failed with status: ${response.statusCode}.');
     }
-    waitData();
   }
 
-  void getDataUsersFromJson() async {
+  Future <void> getDataUsersFromJson() async {
     List dataUsers = json.decode(await rootBundle.loadString('assets/DataUsers.json'));
 
     usersMap = Map.fromIterable(dataUsers,
         key: (item) => item["user"],
         value: (item) => item["password"]);
 
-    waitData();
+    print("OK - getDataUsersFromJson()");
   }
 
-  void delay() async {
+  Future<void> delay() async {
     await Future.delayed(const Duration(seconds: 2));
-    _delay = false;
-    waitData();
+
+    print("OK - delay()");
   }
 
   void waitData() {
-    if (itemsList != null && usersMap != null && _delay == false) {
-
-      Navigator.pushReplacementNamed(context, '/login',
-          arguments: DataFromJson(itemsList, usersMap),
-      );
-    }
+    Navigator.pushReplacementNamed(context, '/login',
+      arguments: DataFromJson(itemsList, usersMap),
+    );
   }
+
 
   @override
   void initState(){
     super.initState();
-    delay();
-    getDataItemsFromJson();
-    getDataUsersFromJson();
+
+    Future.wait([getDataItemsFromJson(), getDataUsersFromJson(), delay()])
+        .then( (List result) => waitData());
   }
 
   @override
